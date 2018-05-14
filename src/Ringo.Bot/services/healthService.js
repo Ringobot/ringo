f5baf07337daef4c3129adcd187d997a0bdddfd9
Daniel Larsen <dalars@microsoft.com>
2018-05-14 20:42:43 +1200

    #428 table storage health

--- src/Ringo.Bot/services/healthService.js
+++ src/Ringo.Bot/services/healthService.js
@@ -8,15 +8,44 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const _table = require("./tablestorage");
 function getHealth(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         // version
         var packageJson = require('../package.json');
         let result = {
-            Status: 'Ok',
+            Status: 'Up',
+            Health: 1.0,
             Version: packageJson.version,
-            DateTime: new Date()
+            DateTime: new Date(),
+            Services: []
         };
+        // table storage
+        try {
+            let start = process.hrtime();
+            let tableHealth = yield _table.get('UserSpotifyAuth', _table.createQuery().top(1));
+            let elapsed = process.hrtime(start);
+            let elapsedMs = (elapsed[0] * 1000) + (elapsed[1] / 1000000);
+            let health = elapsedMs < 100 ? ['Up', 1.0] : elapsedMs < 500 ? ['Trouble', 0.5] : ['Down', 0.1];
+            result.Services.push({
+                Name: "Table Storage",
+                Status: health[0],
+                Health: health[1],
+                Elapsed: elapsedMs
+            });
+        }
+        catch (e) {
+            console.error(e);
+            result.Services.push({
+                Name: "Table Storage",
+                Status: "Down",
+                Message: "Error",
+                Health: 0.0
+            });
+        }
+        // Status reflects table storage status (v1)
+        result.Status = result.Services[0].Status;
+        result.Health = result.Services[0].Health;
         res.send(result);
         return next();
     });
--- src/Ringo.Bot/services/healthService.ts
+++ src/Ringo.Bot/services/healthService.ts
@@ -1,12 +1,46 @@
+import _table = require('./tablestorage')
+
 export async function getHealth(req, res, next) {
     // version
     var packageJson = require('../package.json')
-    
+
     let result = {
-        Status: 'Ok',
+        Status: 'Up',
+        Health: 1.0,
         Version: packageJson.version,
-        DateTime: new Date()
+        DateTime: new Date(),
+        Services:[]
     }
+
+    // table storage
+    try {
+        let start = process.hrtime()
+        let tableHealth = await _table.get('UserSpotifyAuth', _table.createQuery().top(1))
+        let elapsed = process.hrtime(start);
+        let elapsedMs = (elapsed[0] * 1000) + (elapsed[1] / 1000000)
+
+        let health = elapsedMs < 100 ? ['Up', 1.0] : elapsedMs < 500 ? ['Trouble', 0.5] : ['Down', 0.1]
+
+        result.Services.push({
+            Name: "Table Storage",
+            Status: health[0],
+            Health: health[1],
+            Elapsed: elapsedMs
+        })
+
+    } catch (e) {
+        console.error(e)
+        result.Services.push({
+            Name: "Table Storage",
+            Status: "Down",
+            Message: "Error",
+            Health: 0.0
+        })
+    }
+
+    // Status reflects table storage status (v1)
+    result.Status = result.Services[0].Status
+    result.Health = result.Services[0].Health
 
     res.send(result)
     return next()
