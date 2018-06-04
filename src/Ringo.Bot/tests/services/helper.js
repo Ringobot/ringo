e81659326307f81a0361dc71d1d53dd11ada4f6d
Daniel Larsen <daniel@larsen.nz>
2018-06-04 15:57:21 +1200

    Removing JS files from repo based on some good advice

--- src/Ringo.Bot/tests/services/helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-const crypto = require("crypto");
-function newGuid() {
-    return crypto.randomBytes(16).toString("hex");
-}
-exports.newGuid = newGuid;
