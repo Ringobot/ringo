0eeeab49dfc59cd98f87c3fe8810a2372a221be0
Matt Simpson <masim@microsoft.com>
2018-06-22 11:10:19 +1200

    removing npm attempt

--- src/Ringo.Services.TypeScript/services/spotifyhelper.ts
+++
@@ -1,9 +0,0 @@
-export function validateEnvVars() {
-    if (process.env.SpotifyApiClientId == undefined || process.env.SpotifyApiClientSecret == undefined) {
-        throw new Error("spotify.js requires environment vars \"SpotifyApiClientId\" and \"SpotifyApiClientSecret\".");
-    }
-
-    if (process.env.SpotifyAuthRedirectUri == undefined) {
-        throw new Error("spotify.js requires environment var \"SpotifyAuthRedirectUri\".");
-    }
-}
