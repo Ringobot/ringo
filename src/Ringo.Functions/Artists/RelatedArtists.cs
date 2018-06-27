6089e248122603f508c7dec40019ec486c8dc5c6
Matt Simpson <masim@microsoft.com>
2018-06-27 13:49:08 +1200

    Added Durable Function to replace RelatedArtists

---
+++ src/Ringo.Functions/Artists/RelatedArtists.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using Ringo.Common.Heplers;
+using Ringo.Common.Models;
+
+namespace Ringo.Functions.Artists
+{
+    public static class RelatedArtists
+    {
+        [FunctionName("RelatedArtists")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] DurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+            var entity = context.GetInputAsJson();
+            List<EntityRelationship> msgObj = JsonConvert.DeserializeObject<List<EntityRelationship>>(entity["Result"].ToString());
+
+            foreach (EntityRelationship er in msgObj)
+            {
+                outputs.Add(await context.CallActivityAsync<string>("RelatedArtists_CreateRelationship", er));
+            }
+
+            return outputs;
+        }
+
+        [FunctionName("RelatedArtists_CreateRelationship")]
+        public static async Task<string> CreateRelationship([ActivityTrigger] EntityRelationship entityRelationship, TraceWriter log)
+        {
+            await GraphHelper.CreateRelationship(entityRelationship);
+            return $"Added: {entityRelationship.FromVertex.Name}, {entityRelationship.Relationship}, To: {entityRelationship.ToVertex.Name}";
+        }
+
+        [FunctionName("RelatedArtists_HttpStart")]
+        public static async Task HttpStart(
+            [ServiceBusTrigger("graph", "addRelationship")]string msg,
+            [OrchestrationClient]DurableOrchestrationClient starter,
+            TraceWriter log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("RelatedArtists", msg);
+
+            log.Info($"Started orchestration with ID = '{instanceId}'.");
+
+            //return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
--- src/Ringo.Functions/Cosmos/TopicGraphCreateRelationship.cs
+++ src/Ringo.Functions/Cosmos/TopicGraphCreateRelationship.cs
@@ -1,20 +1,20 @@
-using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
-using Ringo.Common.Models;
-using Ringo.Common.Heplers;
+//using System.Threading.Tasks;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Host;
+//using Newtonsoft.Json;
+//using Ringo.Common.Models;
+//using Ringo.Common.Heplers;
 
-namespace Ringo.Functions
-{
-    public static class TopicGraphCreateRelationship
-    {
-        [FunctionName("TopicGraphCreateRelationship")]
-        public static async Task Run([ServiceBusTrigger("graph", "addRelationship")]string msg, TraceWriter log)
-        {
-            if (log != null) log.Info(msg);
-            EntityRelationship msgObj = JsonConvert.DeserializeObject<EntityRelationship>(msg);
-            await GraphHelper.CreateRelationship(msgObj);
-        }
-    }
-}
+//namespace Ringo.Functions
+//{
+//    public static class TopicGraphCreateRelationship
+//    {
+//        [FunctionName("TopicGraphCreateRelationship")]
+//        public static async Task Run([ServiceBusTrigger("graph", "addRelationship")]string msg, TraceWriter log)
+//        {
+//            if (log != null) log.Info(msg);
+//            EntityRelationship msgObj = JsonConvert.DeserializeObject<EntityRelationship>(msg);
+//            await GraphHelper.CreateRelationship(msgObj);
+//        }
+//    }
+//}
